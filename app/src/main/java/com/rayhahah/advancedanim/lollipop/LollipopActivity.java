ced9b52e42f9557c46f13acf2c0ce48462a8b81b
Rayhahah <leizhkkk@126.com>
2017-04-21 16:56:18 +0800

    Update Android Animation Lollipop!

--- app/src/main/java/com/rayhahah/advancedanim/lollipop/LollipopActivity.java
+++ app/src/main/java/com/rayhahah/advancedanim/lollipop/LollipopActivity.java
@@ -1,18 +1,68 @@
 package com.rayhahah.advancedanim.lollipop;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ActivityOptions;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Pair;
+import android.view.View;
+import android.widget.Button;
 
 import com.rayhahah.advancedanim.R;
 
 public class LollipopActivity extends AppCompatActivity {
 
+    private Intent mIntent;
+    private Button btnNormal;
+    private Button btnExplode;
+    private Button btnSlide;
+    private Button btnFade;
+    private View ivImageI;
+    private View ivImageIi;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lollipop);
+        initView();
+    }
 
+    public void btnClick(View view) {
+        mIntent = new Intent(this, TestAnimActivity.class);
+        int animType = 0;
+        switch (view.getId()) {
+            case R.id.btn_normal:
+                animType = TestAnimActivity.ANIM_NORMAL;
+                break;
+            case R.id.btn_explode:
+                animType = TestAnimActivity.ANIM_EXPLODE;
+                break;
+            case R.id.btn_slide:
+                animType = TestAnimActivity.ANIM_SLIDE;
+                break;
+            case R.id.btn_fade:
+                animType = TestAnimActivity.ANIM_FADE;
+                break;
+        }
+        mIntent.putExtra(TestAnimActivity.TAG_ANIM, animType);
 
+        if (animType == TestAnimActivity.ANIM_NORMAL)
+            startActivity(mIntent);
+        else
+            startActivity(mIntent,
+                    ActivityOptions.makeSceneTransitionAnimation(this,
+                            Pair.create(ivImageI, "one"),
+                            Pair.create(ivImageIi, "two"))
+                            .toBundle());
 
+    }
+
+    private void initView() {
+        btnNormal = (Button) findViewById(R.id.btn_normal);
+        btnExplode = (Button) findViewById(R.id.btn_explode);
+        btnSlide = (Button) findViewById(R.id.btn_slide);
+        btnFade = (Button) findViewById(R.id.btn_fade);
+        ivImageI = findViewById(R.id.iv_image_i);
+        ivImageIi = findViewById(R.id.iv_image_ii);
     }
 }
--- app/src/main/java/com/rayhahah/advancedanim/lollipop/TestAnimActivity.java
+++ app/src/main/java/com/rayhahah/advancedanim/lollipop/TestAnimActivity.java
@@ -1,15 +1,99 @@
 package com.rayhahah.advancedanim.lollipop;
 
-import android.support.v7.app.AppCompatActivity;
+import android.animation.Animator;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.transition.Explode;
+import android.transition.Fade;
+import android.transition.Slide;
+import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.view.Window;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.rayhahah.advancedanim.R;
 
 public class TestAnimActivity extends AppCompatActivity {
 
+
+    public static final String TAG_ANIM = "TAG_ANIM";
+    public static int ANIM_NORMAL = 0;
+    public static int ANIM_EXPLODE = 1;
+    public static int ANIM_SLIDE = 2;
+    public static int ANIM_FADE = 3;
+    private ImageView ivImageI;
+    private ImageView ivImageIi;
+    private LinearLayout llRoot;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_CONTENT_TRANSITIONS);
         setContentView(R.layout.activity_test_anim);
+        setTransition(getIntent().getIntExtra(TAG_ANIM, 0));
+        initView();
+
+        llRoot.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
+            @Override
+            public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
+                v.removeOnLayoutChangeListener(this);
+                Animator circularReveal = ViewAnimationUtils.createCircularReveal(
+                        llRoot,
+                        llRoot.getWidth() / 2,
+                        llRoot.getHeight() / 2,
+                        50,
+                       1000);
+                circularReveal.setInterpolator(new AccelerateDecelerateInterpolator());
+                circularReveal.setDuration(1000);
+                circularReveal.start();
+            }
+        });
+        ivImageI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Animator circularReveal = ViewAnimationUtils.createCircularReveal(
+                        ivImageI,
+                        0,
+                        0,
+                        0,
+                        ivImageI.getWidth()+159);
+                circularReveal.setInterpolator(new AccelerateDecelerateInterpolator());
+                circularReveal.setDuration(1000);
+                circularReveal.start();
+            }
+        });
+    }
+
+    /**
+     * 设置动画过渡模式
+     *
+     * @param animType
+     */
+    private void setTransition(int animType) {
+
+        switch (animType) {
+            case 0:
+                break;
+            case 1:
+                getWindow().setEnterTransition(new Explode());
+                getWindow().setExitTransition(new Explode());
+                break;
+            case 2:
+                getWindow().setEnterTransition(new Slide());
+                getWindow().setExitTransition(new Slide());
+                break;
+            case 3:
+                getWindow().setEnterTransition(new Fade());
+                getWindow().setExitTransition(new Fade());
+                break;
+        }
+    }
+
+    private void initView() {
+        ivImageI = (ImageView) findViewById(R.id.iv_image_i);
+        ivImageIi = (ImageView) findViewById(R.id.iv_image_ii);
+        llRoot = (LinearLayout) findViewById(R.id.ll_root);
     }
 }
